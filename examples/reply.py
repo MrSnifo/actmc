b052fe1f9dc7da4f94c1b6d0cf3acf2b01a4146e
Snifo <snifo@mail.com>
2025-07-17 02:08:07 +0100

    examples
    - updated to on_message

--- examples/reply.py
+++ examples/reply.py
@@ -5,30 +5,13 @@ client = Client('Steve')
 
 @client.event
 async def on_ready() -> None:
-    """
-    Called when the bot has connected and is ready to receive events.
-    """
-    print("Bot connected!")
+    """Called when connected and ready"""
+    print(f"Connected as {client.user.username}")
 
-async def handle_message(message: Message) -> None:
-    """Handles incoming chat or system messages."""
+@client.event
+async def on_message(message: Message) -> None:
+    """Called for both chat and system messages."""
     if '!ping' in message.to_plain_text().lower():
         await client.send_message('Pong!')
-
-@client.event
-async def on_system_message(message: Message) -> None:
-    """
-    Called when a system message is received.
-    Some servers send chat messages as system messages, so listen here too.
-    """
-    await handle_message(message)
-
-@client.event
-async def on_chat_message(message: Message) -> None:
-    """
-    Called when a chat message is received.
-    Listen here in case the server sends chat normally.
-    """
-    await handle_message(message)
 
 client.run('localhost', 25565)
